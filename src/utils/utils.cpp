WTEngineer/wxUIEditor

commit bfca4ffc176a7cb94c41e996321e81c3a5396459

International (#84)
* Update string displays to show utf8 characters

Since we compile with /utf-8, ALL char strings are utf8, so display them as such in the debugger.

* Switch std::string_view to ttlib::cview

src/utils/utils.cpp
@@ -14,8 +14,8 @@
 
 #include <ttmultistr.h>  // multistr -- Breaks a single string into multiple strings
 
-#include "node_creator.h"   // NodeCreator class
-#include "utils.h"          // Utility functions that work with properties
+#include "node_creator.h"  // NodeCreator class
+#include "utils.h"         // Utility functions that work with properties
 
 ttlib::cstr DoubleToStr(double val)
 {
@@ -29,7 +29,7 @@ ttlib::cstr DoubleToStr(double val)
     return result;
 }
 
-ttlib::cstr ClearPropFlag(std::string_view flag, std::string_view currentValue)
+ttlib::cstr ClearPropFlag(ttlib::cview flag, ttlib::cview currentValue)
 {
     ttlib::cstr result;
     if (flag.empty() || currentValue.empty())
@@ -51,7 +51,7 @@ ttlib::cstr ClearPropFlag(std::string_view flag, std::string_view currentValue)
     return result;
 }
 
-ttlib::cstr ClearMultiplePropFlags(std::string_view flags, std::string_view currentValue)
+ttlib::cstr ClearMultiplePropFlags(ttlib::cview flags, ttlib::cview currentValue)
 {
     ttlib::cstr result;
     if (flags.empty() || currentValue.empty())
@@ -85,7 +85,7 @@ ttlib::cstr ClearMultiplePropFlags(std::string_view flags, std::string_view curr
     return result;
 }
 
-ttlib::cstr SetPropFlag(std::string_view flag, std::string_view currentValue)
+ttlib::cstr SetPropFlag(ttlib::cview flag, ttlib::cview currentValue)
 {
     ttlib::cstr result(currentValue);
     if (flag.empty())
@@ -107,7 +107,7 @@ ttlib::cstr SetPropFlag(std::string_view flag, std::string_view currentValue)
     return result;
 }
 
-bool isPropFlagSet(std::string_view flag, std::string_view currentValue)
+bool isPropFlagSet(ttlib::cview flag, ttlib::cview currentValue)
 {
     if (flag.empty() || currentValue.empty())
     {
@@ -125,7 +125,7 @@ bool isPropFlagSet(std::string_view flag, std::string_view currentValue)
     return false;
 }
 
-wxPoint ConvertToPoint(std::string_view value)
+wxPoint ConvertToPoint(ttlib::cview value)
 {
     wxPoint pt { -1, -1 };
     if (value.size())
@@ -143,7 +143,7 @@ wxPoint ConvertToPoint(std::string_view value)
     return pt;
 }
 
-wxSize ConvertToSize(std::string_view value)
+wxSize ConvertToSize(ttlib::cview value)
 {
     wxSize size { -1, -1 };
     if (value.size())
@@ -175,7 +175,7 @@ ttlib::cstr ConvertSizeToString(const wxSize& size)
     return str;
 }
 
-int ConvertBitlistToInt(std::string_view list)
+int ConvertBitlistToInt(ttlib::cview list)
 {
     int result = 0;
     if (list.size())
@@ -249,11 +249,11 @@ ttlib::cstr ConvertSystemColourToString(long colour)
     return str;
 }
 
-wxSystemColour ConvertToSystemColour(std::string_view value)
+wxSystemColour ConvertToSystemColour(ttlib::cview value)
 {
-// clang-format off
-    #define IS_SYSCOLOUR(name) if (value == #name) return name;
-    #define ELSE_IS_SYSCOLOUR(name) else if (value == #name) return name;
+    // clang-format off
+    #define IS_SYSCOLOUR(name) if (value.is_sameas(#name)) return name;
+    #define ELSE_IS_SYSCOLOUR(name) else if (value.is_sameas(#name)) return name;
 
     IS_SYSCOLOUR(wxSYS_COLOUR_SCROLLBAR)
 
@@ -295,7 +295,7 @@ wxSystemColour ConvertToSystemColour(std::string_view value)
     // clang-format on
 }
 
-wxColour ConvertToColour(std::string_view value)
+wxColour ConvertToColour(ttlib::cview value)
 {
     // check for system colour
     if (ttlib::is_sameprefix(value, "wx"))
@@ -443,7 +443,7 @@ ttlib::cstr CreateEscapedText(ttlib::cview str)
     return result;
 }
 
-std::vector<std::string> ConvertToArrayString(std::string_view value)
+std::vector<std::string> ConvertToArrayString(ttlib::cview value)
 {
     std::vector<std::string> array;
     if (value.empty())
@@ -452,9 +452,10 @@ std::vector<std::string> ConvertToArrayString(std::string_view value)
     auto pos = parse.ExtractSubString(value);
     array.emplace_back(parse);
 
-    for (value = ttlib::stepover(value.data() + pos); value.size(); value = ttlib::stepover(value.data() + pos))
+    for (std::string_view tmp_value = ttlib::stepover(value.data() + pos); tmp_value.size();
+         tmp_value = ttlib::stepover(value.data() + pos))
     {
-        pos = parse.ExtractSubString(value);
+        pos = parse.ExtractSubString(tmp_value);
         array.emplace_back(parse);
     }
 
@@ -463,7 +464,7 @@ std::vector<std::string> ConvertToArrayString(std::string_view value)
 
 // Add C++ escapes around any characters the compiler wouldn't accept as a normal part of a string. Used when generating
 // code.
-ttlib::cstr ConvertToCodeString(std::string_view text)
+ttlib::cstr ConvertToCodeString(ttlib::cview text)
 {
     ttlib::cstr result;
 

src/bitmaps.cpp
@@ -16,7 +16,7 @@
 
 #include "bitmaps.h"
 
-wxImage convertPngHeader(std::string_view filename)
+wxImage convertPngHeader(ttlib::cview filename)
 {
     wxImage image;
 
@@ -142,7 +142,7 @@ wxImage convertPngHeader(std::string_view filename)
     return image;
 }
 
-wxImage GetHeaderImage(std::string_view filename)
+wxImage GetHeaderImage(ttlib::cview filename)
 {
     wxImage image;
 

src/bitmaps.h
@@ -19,12 +19,12 @@ void InitializeIcons();
 // images created by the EmbedImage class or by bin2c.
 //
 // Call image.isOk() to verify that the image was loaded.
-wxImage convertPngHeader(std::string_view filename);
+wxImage convertPngHeader(ttlib::cview filename);
 
 // Converts an image stored as an unsigned char array in a header file. It's designed to read
 // header files created by wxUiEditor -- any other generated header file might or might not
 // work.
-wxImage GetHeaderImage(std::string_view filename);
+wxImage GetHeaderImage(ttlib::cview filename);
 
 // Returns wxImage(default.xpm) if string cannot be found. Default.xpm is a question mark.
 wxImage GetXPMImage(ttlib::cview name);

src/mainframe.cpp
@@ -1016,7 +1016,7 @@ void MainFrame::ToggleBorderFlag(Node* node, int border)
     ModifyProperty(propFlag, value);
 }
 
-void MainFrame::ModifyProperty(NodeProperty* prop, std::string_view value)
+void MainFrame::ModifyProperty(NodeProperty* prop, ttlib::cview value)
 {
     if (prop && value != prop->as_cview())
     {

src/mainframe.h
@@ -110,7 +110,7 @@ public:
     void RemoveNode(Node* node, bool isCutMode);
 
     // Call this MainFrame version if you don't have access to a node.
-    void ModifyProperty(NodeProperty* prop, std::string_view value);
+    void ModifyProperty(NodeProperty* prop, ttlib::cview value);
 
     void ChangeAlignment(Node* node, int align, bool vertical);
 

src/nodes/node.cpp
@@ -741,7 +741,7 @@ void Node::ModifyProperty(ttlib::cview name, int value)
     }
 }
 
-void Node::ModifyProperty(ttlib::cview name, std::string_view value)
+void Node::ModifyProperty(ttlib::cview name, ttlib::cview value)
 {
     auto prop = get_prop_ptr(name);
     if (prop && value != prop->as_cview())
@@ -760,7 +760,7 @@ void Node::ModifyProperty(NodeProperty* prop, int value)
     }
 }
 
-void Node::ModifyProperty(NodeProperty* prop, std::string_view value)
+void Node::ModifyProperty(NodeProperty* prop, ttlib::cview value)
 {
     if (prop && value != prop->as_cview())
     {

src/nodes/node.h
@@ -171,13 +171,13 @@ public:
     bool CreateToolNode(const ttlib::cstr& name);
 
     // This will modify the property and fire a EVT_NodePropChange event
-    void ModifyProperty(ttlib::cview name, std::string_view value);
+    void ModifyProperty(ttlib::cview name, ttlib::cview value);
 
     // This will modify the property and fire a EVT_NodePropChange event
     void ModifyProperty(ttlib::cview name, int value);
 
     // This will modify the property and fire a EVT_NodePropChange event
-    void ModifyProperty(NodeProperty* prop, std::string_view value);
+    void ModifyProperty(NodeProperty* prop, ttlib::cview value);
 
     // This will modify the property and fire a EVT_NodePropChange event
     void ModifyProperty(NodeProperty* prop, int value);

src/nodes/node_decl.cpp
@@ -162,7 +162,7 @@ bool NodeDeclaration::IsSubclassOf(const std::string& classname) const
     return false;
 }
 
-bool NodeDeclaration::isSubclassOf(std::string_view classname) const
+bool NodeDeclaration::isSubclassOf(ttlib::cview classname) const
 {
     if (m_classname.is_sameas(classname))
     {

commit 01f71c6d9d6d7d5b5723be3ea170e261204361dd

Fix bug introduced by PR #84

src/utils/utils.cpp
@@ -452,8 +452,8 @@ std::vector<std::string> ConvertToArrayString(ttlib::cview value)
     auto pos = parse.ExtractSubString(value);
     array.emplace_back(parse);
 
-    for (std::string_view tmp_value = ttlib::stepover(value.data() + pos); tmp_value.size();
-         tmp_value = ttlib::stepover(value.data() + pos))
+    for (auto tmp_value = ttlib::stepover(value.data() + pos); tmp_value.size();
+         tmp_value = ttlib::stepover(tmp_value.data() + pos))
     {
         pos = parse.ExtractSubString(tmp_value);
         array.emplace_back(parse);